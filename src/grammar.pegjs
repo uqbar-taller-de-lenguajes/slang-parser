a26b0b8a81069d333fc05e1f977d16050bf4c75b
Charlyzzz <erwincdl@gmail.com>
2017-08-17 21:56:19 -0300

    Conditional branch

--- src/grammar.pegjs
+++ src/grammar.pegjs
@@ -1,9 +1,36 @@
+{
+  Object.prototype.put = function put(key, value) {
+    return { ...this, [key]: value }
+  }
+
+  Object.prototype.safePut = function safePut(key, value) {
+    return (value === undefined)? this : this.put(key, value)
+  }
+
+  let BaseNode = function BaseNode(type){
+    return { type: type }
+  }
+
+  function booleanNode(value) {
+    let boolean = value === 'true'
+    return BaseNode('boolean').put('value', boolean)
+  }
+
+  function conditionalNode(condition, then, otherwise) {    
+    return BaseNode('if')
+      .put('condition', condition)
+      .put('then', then)
+      .safePut('otherwise', otherwise[3])
+  }
+}
+
 slang = term
+
+_ = __?
+__ = [ \t\r\n]+
 
 term = boolean / conditional
 
-boolean = true / false
-true = 'true' { return 1 }
-false = 'false' {}
+boolean = value:('true' / 'false') { return booleanNode(value) }
 
-conditional = 'if' condition: term 'then' consecuence: term otherwise: term? {}
+conditional = 'if' __ condition:(term) __ 'then' __ then:(term) otherwise:(__ 'else' __ term)? { return conditionalNode(condition, then, otherwise || []) }
--- test/parser.test.js
+++ test/parser.test.js
@@ -4,15 +4,45 @@ import { expect } from 'chai'
 import parse from '../src/parser'
 
 describe('parser', () => {
-  describe('example', () => {
-    it('parses when it should', () => {
-      expect(parse('true')).to.equal(1)
+
+  describe('booleans', () => {
+
+    it('parses true into a boolean node', () => {
+      expect(parse('true')).to.deep.equal({ type: 'boolean', value: true })
     })
 
-    it('fails when it should', () => {
-      expect(() => parse('abc')).to.throw()
+    it('parses false into a boolean node', () => {
+      expect(parse('false')).to.deep.equal({ type: 'boolean', value: false })
     })
-
   })
 
+  describe('conditional', () => {
+
+    describe('without else', () => {
+
+      it('parses an if into an if node', () => {
+        let trueNode = { type: 'boolean', value: true }
+        expect(parse('if true then true')).to.deep.equal({ type: 'if', condition: trueNode, then: trueNode })
+      })
+    })
+
+    describe('with else', () => {
+      it('parses an if into an if node', () => {
+        let trueNode = { type: 'boolean', value: true }
+        let falseNode = { ...trueNode, value: false }
+        expect(parse('if true then true else false')).to.deep.equal({ type: 'if', condition: trueNode, then: trueNode, otherwise: falseNode })
+      })
+    })
+
+    it('handles if anidation', () => {
+      let trueNode = { type: 'boolean', value: true }
+      let falseNode = { ...trueNode, value: false }
+      let insideIfNode = { type: 'if', condition: trueNode, then: trueNode }
+      expect(parse('if if true then true then true else false')).to.deep.equal({ type: 'if', condition: insideIfNode, then: trueNode, otherwise: falseNode })
+    })
+  })
+
+  it('fails when it should', () => {
+    expect(() => parse('abc')).to.throw()
+  })
 })
